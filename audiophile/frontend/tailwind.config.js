b202f4fd77932c6568357e73592473a39b517fcf
James <james.mcgrory135@gmail.com>
2023-06-02 19:06:36 +1000

    added Tailwind color classes and font family from the design file

--- audiophile/frontend/tailwind.config.js
+++ audiophile/frontend/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'raw-siena': '#D87D4A',
+        'hit-pink': '#FBAF85',
+        'cod-gray': '#101010',
+        'seashell': '#F1F1F1'
+      },
+      //fontFamily: {
+        //'jost' : ['Jost', 'sans-serif'],
+     // }
+    },
   },
   plugins: [],
 }
